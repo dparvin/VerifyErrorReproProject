58683243e4b9a492de3c3410716a39c97794d487
David Parvin <dwparvin@cableone.net>
2021-07-07 19:02:41 -0600

    Add project files.

---
+++ VerifyErrorReproProject/BasicItem.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Xml.Linq
+
+Public Class BasicItem
+
+    Public Sub New()
+        Name = String.Empty
+        Type = 0
+        Setting = <root/>
+    End Sub
+
+    Public Property Name As String
+    Public Property Type As Integer
+    Public Property Setting As XElement
+
+End Class
---
+++ VerifyErrorReproProjectTest/BasicItemTest.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Xml.Linq
+Imports VerifyErrorReproProject
+Imports VerifyTests
+Imports VerifyXunit
+Imports Xunit
+Imports Xunit.Abstractions
+
+Public Class BasicItemTest
+
+    Inherits VerifyBase
+    Private Property Output As ITestOutputHelper
+
+    Private Property Settings As VerifySettings
+
+    Public Sub New(ByVal outputHelper As ITestOutputHelper)
+
+        MyBase.New()
+
+        Output = outputHelper
+
+    End Sub
+
+    <Fact>
+    Public Sub BasicItemTest()
+
+        Dim obj As New BasicItem()
+
+        With obj
+            .Name = "TestItem"
+            .Type = 55
+            .Setting.Add(New XElement("TestItem"))
+        End With
+
+        Verify(obj)
+
+    End Sub
+
+End Class
