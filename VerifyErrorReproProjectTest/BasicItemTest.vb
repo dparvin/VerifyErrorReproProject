29710e434c0daf43ca4753d3a27fde5fc71148ce
David Parvin <dwparvin@cableone.net>
2021-07-09 19:54:32 -0600

    Added Verify to test

--- VerifyErrorReproProjectTest/BasicItemTest.vb
+++ VerifyErrorReproProjectTest/BasicItemTest.vb
@@ -1,13 +1,21 @@
 ﻿Imports System.Xml.Linq
 Imports VerifyErrorReproProject
+Imports VerifyTests
+Imports VerifyXunit
 Imports Xunit
 Imports Xunit.Abstractions
 
 Public Class BasicItemTest
 
+    Inherits VerifyBase
+
     Private Property Output As ITestOutputHelper
 
+    Private Property Settings As VerifySettings
+
     Public Sub New(ByVal outputHelper As ITestOutputHelper)
+
+        MyBase.New()
 
         Output = outputHelper
 
@@ -26,6 +34,15 @@ Public Class BasicItemTest
 
         Output.WriteLine("obj: " & obj.ToString())
 
+        VerifyObject(obj)
+
     End Sub
+
+    Private Async Sub VerifyObject(Item As Object)
+
+        Await Verify(Item, Settings)
+
+    End Sub
+
 
 End Class
